93c5cd2482fb9fdaf45884af1085fbd540137d1a
Your New Name <nourtomeh17@gmail.com>
2026-02-28 20:31:16 +0200

    Update BE1 add /health route add .env.example split scripts into run &dev add real DB connection

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -12,6 +12,7 @@ var app = express();
 
 app.use(cors());
 
+const healthRouter = require('./Modules/health');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('', healthRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
