eae026c56a28b07dd9db1bc52e35c8575b9faf26
Lubna <Lubna.jaradat14@gmail.com>
2026-03-05 00:23:44 +0200

    refactor: move findUserByEmail to user.service

--- BackEnd/Modules/auth/auth.service.js
+++ BackEnd/Modules/auth/auth.service.js
@@ -1,33 +1,30 @@
-const prisma  = require('../prismaClient');
+const prisma = require("../prismaClient");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const { findUserByEmail } = require("../user/user.service");
+const { signAccessToken, signRefreshToken } = require("./utils/jwt.utils");
 
-
-
-const findUserByEmail = async (email) => {
-    return await prisma.user.findUnique({
-        where: { email }
-    })
-};
 
 const registerUser = async (name, email, hashedPassword) => {
-    const newUser = await prisma.user.create({
-        data :{
-            name,
-            email,
-            passwordHash: hashedPassword
-        },
-        select: {
-            id: true,
-            name: true,
-            email: true
-        }
-    })
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash: hashedPassword,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+    },
+  });
 
-    return {
-        ...newUser,
-        id: newUser.id.toString()
-    };
+  return {
+    ...newUser,
+    id: newUser.id.toString(),
+  };
 };
 module.exports = {
-    findUserByEmail,
-    registerUser
+  findUserByEmail,
+  registerUser,
 };
