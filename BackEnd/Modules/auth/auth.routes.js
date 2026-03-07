8bea4ec5d208bb22d130a65da179cd959a5b7197
Rand Haymouni <rand54144@gmail.com>
2026-03-08 00:37:45 +0200

    some changes in BE

--- BackEnd/Modules/auth/auth.routes.js
+++ BackEnd/Modules/auth/auth.routes.js
@@ -6,9 +6,9 @@ const registerUserSchema = require("./schema/RegisterUser");
 const loginSchema = require("./schema/login.schema");
 
 
-router.post('/register', validate({body: registerUserSchema}) , authController.register )
+router.post('/register', validate({ body: registerUserSchema }), authController.register)
 
-router.post('/login', validate({body: loginSchema}), authController.login);
+router.post('/login', validate({ body: loginSchema }), authController.login);
 
 module.exports = router;
 
