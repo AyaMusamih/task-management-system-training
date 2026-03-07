5ce58248dc9151b6700a2573b2d1d54d06e30405
Rand Haymouni <rand54144@gmail.com>
2026-03-08 00:24:35 +0200

    Create dashboards for user and admin roles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,16 @@ import { ToastContainer } from "react-toastify";
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import Login from './pages/auth/Login'
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
+import Signup from './pages/auth/Signup';
 import Profile from './pages/Profile';
 import ChangePassword from './pages/ChangePassword';
 import Reports from './pages/admin/Reports';
 import AllTickets from './pages/admin/AllTickets';
 import TaskManagement from './pages/admin/TaskManagement';
 import DeletedTickets from './pages/admin/DeletedTickets ';
+import AdminDashboard from './pages/admin/AdminDashboard';
 import MyTasks from './pages/user/MyTasks';
+import UserDashboard from './pages/user/UserDashboard';
 import TicketDetailsModal from './components/TicketDetailsModal';
 import NotFound from './pages/NotFound';
 
@@ -25,7 +26,10 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route element={<MainLayout />}>
           <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Dashboard />} >
+          <Route path='/user/dashboard' element={<UserDashboard />} >
+            <Route path='tickets/:id' element={<TicketDetailsModal />} />
+          </Route>
+          <Route path='/admin/dashboard' element={<AdminDashboard />} >
             <Route path='tickets/:id' element={<TicketDetailsModal />} />
           </Route>
           <Route path='/profile' element={<Profile />} />
