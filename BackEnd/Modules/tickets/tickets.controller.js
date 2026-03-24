7eaa9808f3536b188fde49a3ca2a77153cd2d00b
Lubna <Lubna.jaradat14@gmail.com>
2026-03-25 01:46:09 +0200

    feat: implement controller and service logic for creatTickt

--- BackEnd/Modules/tickets/tickets.controller.js
+++ BackEnd/Modules/tickets/tickets.controller.js
@@ -1,32 +1,78 @@
-const ticketService = require("./tickets.service")
-const {attachPermissionFlags} = require("./utils/ticket-premissions.util")
+const ticketService = require("./tickets.service");
+const { attachPermissionFlags } = require("./utils/ticket-premissions.util");
 
 const getTickets = async (req, res, next) => {
-    try {
-        const {
-            view, status, assignee, priority, startDate, endDate, page, limit, sortBy, search
-        } = req.query;
+  try {
+    const {
+      view,
+      status,
+      assignee,
+      priority,
+      startDate,
+      endDate,
+      page,
+      limit,
+      sortBy,
+      search,
+    } = req.query;
 
-        const result = await ticketService.getTickets(
-           req.user, view, status, assignee, priority, startDate, endDate, page, limit, sortBy,search
-        );
-        const resultWithFlags = attachPermissionFlags(result, req.user)
-        
-        res.status(200).json({
-            success: true,
-            ...resultWithFlags
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+    const result = await ticketService.getTickets(
+      req.user,
+      view,
+      status,
+      assignee,
+      priority,
+      startDate,
+      endDate,
+      page,
+      limit,
+      sortBy,
+      search,
+    );
+    const resultWithFlags = attachPermissionFlags(result, req.user);
+
+    res.status(200).json({
+      success: true,
+      ...resultWithFlags,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const addTicket = async (req, res, next) => {
+  try {
+    const {
+      title,
+      description,
+      status,
+      priority,
+      deadline,
+      assigneeId,
+      sprintId,
+    } = req.body;
+    const payload = {
+      title,
+      description,
+      status,
+      priority,
+      deadline: deadline ? new Date(deadline) : null,
+      assigneeId: assigneeId ?? null,
+      sprintId: sprintId ?? null,
+    };
 
-}
+    const newTicket = await ticketService.createTicket(payload, req.user.id)
+    res.status(201).json({
+      message: "Ticket created successfully",
+      data: newTicket,
+    });
 
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    getTickets, 
-    addTicket
-}
+  getTickets,
+  addTicket,
+};
--- BackEnd/Modules/tickets/tickets.service.js
+++ BackEnd/Modules/tickets/tickets.service.js
@@ -1,69 +1,105 @@
 const prisma = require("../prismaClient");
+const { connect } = require("./tickets.route");
 
-const getTickets = async (user, view, status, assignee, priority, startDate, endDate, page, limit, sortBy, search) => {
-    const user_id =BigInt(user.id)
-    const where = {deletedAt: null};
+const getTickets = async (
+  user,
+  view,
+  status,
+  assignee,
+  priority,
+  startDate,
+  endDate,
+  page,
+  limit,
+  sortBy,
+  search,
+) => {
+  const user_id = BigInt(user.id);
+  const where = { deletedAt: null };
 
-    if (user.role !== "ADMIN") {
-        where.OR = [
-            { sprintId: { not: null }, assigneeId: user_id },
-            { status: "SCOPED_BACKLOG", assigneeId: user_id },
-        ];
-    } else {
-        if (assignee) where.assigneeId = BigInt(assignee);
-    }
-        if (view === "sprint") {
-        where.sprintId = {not: null};
-        where.sprint = {is: {isActive: true}};
-    }
-    if (view === "scoped" && !status) where.status = "SCOPED_BACKLOG";
-    if (status) where.status = status;
-    if (priority) where.priority = priority;
-    if (startDate || endDate) {
-        where.createdAt = {};
-        if (startDate) where.createdAt.gte = startDate;
-        if (endDate) where.createdAt.lte = endDate;
-    }
-    if (search) {
-        where.title = {
-            contains: search,
-            mode: "insensitive"
-        };
-    }
-
-    const skip = (page - 1) * limit;
-    const [tickets, total] = await Promise.all([
-        prisma.ticket.findMany({
-            where,
-            skip,
-            take: limit,
-            orderBy: {[sortBy]: "desc"},
-            select: {
-                id: true,
-                title: true,
-                description: true,
-                status: true,
-                priority: true,
-                deadline: true,
-                createdAt: true,
-                updatedAt: true,
-                assignee: {select: {id: true, name: true, email: true}},
-                createdBy: {select: {id: true, name: true}},
-                sprint: {select: {id: true, name: true}},
-            }
-        }),
-        prisma.ticket.count({where})
-    ]);
-    return {
-        items: tickets,
-        paginationMeta: {
-            total,
-            page,
-            limit,
-            totalPages: Math.ceil(total / limit),
-        }
+  if (user.role !== "ADMIN") {
+    where.OR = [
+      { sprintId: { not: null }, assigneeId: user_id },
+      { status: "SCOPED_BACKLOG", assigneeId: user_id },
+    ];
+  } else {
+    if (assignee) where.assigneeId = BigInt(assignee);
+  }
+  if (view === "sprint") {
+    where.sprintId = { not: null };
+    where.sprint = { is: { isActive: true } };
+  }
+  if (view === "scoped" && !status) where.status = "SCOPED_BACKLOG";
+  if (status) where.status = status;
+  if (priority) where.priority = priority;
+  if (startDate || endDate) {
+    where.createdAt = {};
+    if (startDate) where.createdAt.gte = startDate;
+    if (endDate) where.createdAt.lte = endDate;
+  }
+  if (search) {
+    where.title = {
+      contains: search,
+      mode: "insensitive",
     };
+  }
+
+  const skip = (page - 1) * limit;
+  const [tickets, total] = await Promise.all([
+    prisma.ticket.findMany({
+      where,
+      skip,
+      take: limit,
+      orderBy: { [sortBy]: "desc" },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        status: true,
+        priority: true,
+        deadline: true,
+        createdAt: true,
+        updatedAt: true,
+        assignee: { select: { id: true, name: true, email: true } },
+        createdBy: { select: { id: true, name: true } },
+        sprint: { select: { id: true, name: true } },
+      },
+    }),
+    prisma.ticket.count({ where }),
+  ]);
+  return {
+    items: tickets,
+    paginationMeta: {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
+};
+
+const createTicket = async (payload, userId) => {
+  const { sprintId, assigneeId, ...details } = payload;
+  return await prisma.ticket.create({
+    data: {
+      ...details,
+      createdBy: {
+        connect: {
+          id: user_id,
+        },
+      },
+      assignee: assigneeId ? { connect: { assigneeId } } : undefined,
+      sprint: sprintId ? { connect: sprintId } : undefined,
+    },
+    include: {
+      assignee: {
+        select: { id: true, name: true, email: true },
+      },
+      sprint: true,
+    },
+  });
 };
 module.exports = {
-    getTickets
-}
+  getTickets,
+  createTicket,
+};
